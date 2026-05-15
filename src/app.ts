ce2b088e5ad0452745b989ed2d2228b562dbddbc
El1angonzalez <elian_17_05@hotmail.com>
2026-05-15 00:14:56 -0500

    fix cors options

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { Application, Request, Response, NextFunction } from "express";
+import cors, { CorsOptions } from "cors";
 import path from "path";
 
 // ---------------------- IMPORTACIÓN DE RUTAS ----------------------
@@ -13,24 +14,14 @@ import prestamoRoutes from "./routes/prestamo.routes";
 // ---------------------- CONFIGURACIÓN DE APP ----------------------
 const app: Application = express();
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
-  );
+const corsOptions: CorsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  credentials: true
+};
 
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-
-  next();
-});
-
+app.use(cors({ exposedHeaders: ["Content-Disposition"] }));
+app.use(cors(corsOptions));
 app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ extended: true, limit: '20mb' }));
 
