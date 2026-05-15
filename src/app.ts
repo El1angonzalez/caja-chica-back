b55899ddc9dc7fde20c6e6417f9224a21e9d9830
El1angonzalez <elian_17_05@hotmail.com>
2026-05-14 23:56:52 -0500

    fix cors options

--- src/app.ts
+++ src/app.ts
@@ -15,14 +15,15 @@ import prestamoRoutes from "./routes/prestamo.routes";
 const app: Application = express();
 
 const corsOptions: CorsOptions = {
-  origin: ["http://localhost:5173"],
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  origin: "http://localhost:5173",
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 };
 
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
-app.use(cors(corsOptions));
 app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ extended: true, limit: '20mb' }));
 
